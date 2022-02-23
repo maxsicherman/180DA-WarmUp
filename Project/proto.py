f665e2312eb06c0ad1ea59013a09a0626c359fb5
maxsicherman <97261414+maxsicherman@users.noreply.github.com>
2022-02-22 20:10:08 -0800

    Starting to implement mqtt

--- Project/proto.py
+++ Project/proto.py
@@ -3,9 +3,32 @@ import os
 import random
 from pydub import AudioSegment
 from pydub.playback import play
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connection returned result: "+str(rc))
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("Unexpected Disconnect")
+    else:
+        print("Expected Disconnect")
+
+def on_message(client, userdata, message):
+    print('Received message: "' + str(message.payload) + '" on topic "' +
+        message.topic + '" with QoS ' + str(message.qos))
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+
+client.connect_async("test.mosquitto.org")
+client.loop_start()
 
 on = True
-melodySize = 2
+melodySize = 4
 melody = []
 sounds = ['C4.wav', 'G4.wav'] 
 seed = 0
@@ -18,12 +41,6 @@ while on:
     for i in range(melodySize-1):
         relation[i] = melody[i+1] - melody[i]
 
-    for i in range(melodySize):
-        sound = AudioSegment.from_wav(sounds[melody[i]])
-        play(sound)
-
-    ans = input('Pitch movement (^/v/>): ')
-
     sol = ''
     for i in range(len(relation)):
         if relation[i] < 0:
@@ -33,6 +50,23 @@ while on:
         elif relation[i] > 0:
             sol += '^'
 
+## Publish the melody and correct answer
+
+client.publish('ece180d/test', melody, qos=1)
+client.publish('ece180d/test', sol, qos=1)
+
+## Here's how the keyboard input is processed
+
+    for i in range(melodySize):
+        sound = AudioSegment.from_wav(sounds[melody[i]])
+        play(sound)
+
+## Answer's made on client side instead of this:
+
+#    ans = input('Pitch movement (^/v/>): ')
+    
+## How to store subscribed message?
+#    ans = client.loop_start()
     if ans == sol:
         print('Correct!')
     else:
